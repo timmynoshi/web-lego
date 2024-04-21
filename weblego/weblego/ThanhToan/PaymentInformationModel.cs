fc6ccfd8153547734bd25e4a3e164c9159374abf
NguyenThanhTin <asboperman2507@gmail.com>
2024-04-22 01:19:13 +0700

    Temp5 - đang làm thanh toán online

---
+++ weblego/weblego/ThanhToan/PaymentInformationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DoAnWeb.ThanhToan
+{
+    public class PaymentInformationModel
+    {
+        public string OrderType { get; set; } 
+        public static double Amount { get; set; } 
+        public string OrderDescription { get; set; }
+        public string Name { get; set; }
+    }
+}
+
